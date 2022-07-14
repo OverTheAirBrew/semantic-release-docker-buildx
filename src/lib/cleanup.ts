9c44fb919eaa9e3f32ba7912b234d3e97a4a22a6
Nick Sharp <nick.sharp.87@gmail.com>
2022-07-14 12:28:53 +0100

    fix: fixing cleanup script

--- src/lib/cleanup.ts
+++ src/lib/cleanup.ts
@@ -1,15 +1,19 @@
 import execa = require('execa');
 
 export async function cleanup() {
-  const { stdout } = await execa('docker', [
-    'buildx',
-    'ls',
-    '|',
-    'grep',
-    'dockerx-builder',
-  ]);
+  const { stdout } = await execa('docker', ['buildx', 'ls']);
 
-  if (!stdout) return;
+  if (stdout.indexOf('dockerx-builder') === -1) return;
 
-  await execa('docker', ['dockerx', 'rm', 'dockerx-builder']);
+  await execa('docker', ['buildx', 'rm', 'dockerx-builder']);
 }
+
+Promise.resolve()
+  .then(async () => {
+    await cleanup();
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
